b9982029f55f8d4995c92f44c0d84dabe109b435
blink-blade <micah@blinkblade.net>
2025-02-20 22:35:58 -0500

    step four?

---
+++ db.py
@@ -0,0 +1,36 @@
+import requests
+from sqlalchemy import *
+from sqlalchemy.orm import *
+
+
+engine = create_engine('sqlite:///OHYEAH.db')
+
+Session = sessionmaker(bind=engine)
+db_session = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(50))
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+Base.metadata.create_all(engine)
+
+
+# user1 = User(name='Jimmy', email='Jimmy@gmail.com', password='12345')
+# user2 = User(name='John', email='John@gmail.com', password='123')
+# user3 = User(name='Jeorge', email='Jeorge@gmail.com', password='1234567890')
+# db_session.add(user1)
+# db_session.add(user2)
+# db_session.add(user3)
+# db_session.commit()
+
+# print(db_session.query(User.name).all())
+
+
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask
+
+"""basic Flask app - demo of using a variable in a route"""
+import requests
+from flask import Flask, jsonify
+from db import db_session, User
+
 app = Flask(__name__)
 
 @app.route('/')
-def wut():
-    return 'What are you doing here?'
+def hello():
+    with open('main.html', 'r') as file:
+        return file.read()
 
-@app.route('/hay')
-def hayyy():
-    return '<h1>HAYYY</h1>'
+@app.route('/read/<name>')
+def user(name):
+    user = db_session.query(User).filter_by(name=name).first()
+    return user.as_dict()
+
+@app.route('/read_all/')
+def read_all():
+    users = {}
+    user_list = db_session.query(User).all()
+    for user in user_list:
+        users[user.name] = user.as_dict()
+    return users
+
+@app.route('/post/password=<password>_name=<name>_email=<email>')
+def create(name, password, email):
+    if db_session.query(User).filter_by(name=name).first():
+        return 'User already exists.'
+
+    user = User(name=name, email=email, password=password)
+    db_session.add(user)
+    db_session.commit()
+    return 'User Added'
+
+if __name__ == '__main__':
+    app.run(debug=True)
